071aa00bb3781e497d6339dc11e6d3308415b033
TheFloatingBrain <TheFl@LAPTOP-UMJQFRCR>
2019-06-04 22:13:27 -0400

    Its not crashing and delivering messages... I think thats good.

--- VulkanModel/VulkanModel.cpp
+++ VulkanModel/VulkanModel.cpp
@@ -19,7 +19,7 @@ static VKAPI_ATTR VkBool32 VKAPI_CALL DebugCallBack( VkDebugUtilsMessageSeverity
 	VkDebugUtilsMessageTypeFlagsEXT messageType, const VkDebugUtilsMessengerCallbackDataEXT* callBackData,
 	void* userData )
 {
-	std::cerr << "Error::Vulkan::ValidationLayer: " << callBackData->pMessage << "\n";
+	std::cerr << "Message::Vulkan::ValidationLayer: " << callBackData->pMessage << "\n";
 
 	return VK_FALSE;
 }
@@ -27,7 +27,8 @@ static VKAPI_ATTR VkBool32 VKAPI_CALL DebugCallBack( VkDebugUtilsMessageSeverity
 void Initialize( unsigned int width_, unsigned int height_, std::string title );
 void InitializeGLFW( unsigned int width_, unsigned int height_, std::string title );
 void InitializeVulkan( std::string name );
-VkResult InitializeVulkanDebugLayer();
+void PrintAvailibleExtensions();
+VkResult InitializeVulkanDebugLayer( std::vector< const char* >&& validationLayers );
 bool Update();
 bool GLFWUpdate();
 void Destroy();
@@ -62,58 +63,60 @@ void InitializeGLFW( unsigned int width_, unsigned int height_, std::string titl
 	glfwWindowHint( GLFW_RESIZABLE, GLFW_FALSE );
 	window = glfwCreateWindow( width, height, title.c_str(), nullptr, nullptr );
 }
+void PrintAvailibleExtensions()
+{
+	uint32_t extensionCount = 0;
+	vkEnumerateInstanceExtensionProperties( nullptr, &extensionCount, nullptr );
+	std::vector< VkExtensionProperties > extensions( extensionCount );
+	vkEnumerateInstanceExtensionProperties( nullptr, &extensionCount, extensions.data() );
+	std::cout << "Available Extensions: \n";
+	for(const auto& currentExtension : extensions)
+		std::cout << "\t" << currentExtension.extensionName << "\n";
+}
 void InitializeVulkan( std::string name )
 {
+	std::vector< const char* > validationLayers{ "VK_LAYER_KHRONOS_validation" };
 	applicationInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
 	applicationInfo.pApplicationName = name.c_str();
 	applicationInfo.applicationVersion = VK_MAKE_VERSION( 1, 0, 0 );
 	applicationInfo.pEngineName = "No Engine";
 	applicationInfo.engineVersion = VK_MAKE_VERSION( 1, 0, 0 );
 	applicationInfo.apiVersion = VK_API_VERSION_1_0;
-
 	createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
 	createInfo.pApplicationInfo = &applicationInfo;
-
 	createInfo.ppEnabledExtensionNames = glfwExtensions;
+	PrintAvailibleExtensions();
+	glfwExtensions = glfwGetRequiredInstanceExtensions( &glfwExtensionCount );
+	std::vector< const char* > enabledExtensions( glfwExtensions, glfwExtensions + glfwExtensionCount );
+	createInfo.enabledLayerCount = static_cast< uint32_t >( validationLayers.size() );
+	createInfo.ppEnabledLayerNames = validationLayers.data();
 
-	if( debug == true )
-	{
-		std::cout << "Debug mode, enabling validation layers.\n";
-		InitializeVulkanDebugLayer();
-	}
+	enabledExtensions.push_back( VK_EXT_DEBUG_REPORT_EXTENSION_NAME );
+	enabledExtensions.push_back( VK_EXT_DEBUG_UTILS_EXTENSION_NAME );
 
-	if( vkCreateInstance( &createInfo, nullptr, &instance ) != VK_SUCCESS ) {
-		std::cerr << "Error::InitializeVulkan( std::string ): Failed to create instance.\n";
+	createInfo.enabledExtensionCount = enabledExtensions.size();
+	createInfo.ppEnabledExtensionNames = enabledExtensions.data();
+
+	if(vkCreateInstance( &createInfo, nullptr, &instance ) != VK_SUCCESS) {
+		std::cerr << "Error::InitializeVulkan( std::string ):void: Failed to create instance.\n";
 		return;
 	}
-
-	uint32_t extensionCount = 0;
-
-	vkEnumerateInstanceExtensionProperties( nullptr, &extensionCount, nullptr );
-
-	std::vector< VkExtensionProperties > extensions( extensionCount );
-
-	vkEnumerateInstanceExtensionProperties( nullptr, &extensionCount, extensions.data() );
-
-	std::cout << "Available Extensions: \n";
-
-	for(const auto& currentExtension : extensions)
-		std::cout << "\t" << currentExtension.extensionName << "\n";
-
+	if( debug == true )
+	{
+		std::cout << "Note::InitializeVulkan( std::string ):void: Debug mode, enabling validation layers.\n";
+		auto result = InitializeVulkanDebugLayer( std::move( validationLayers ) );
+		if( result == VK_SUCCESS )
+			std::cout << "Note::InitializeVulkan( std::string ):void: Debug layer creation succesful!\n";
+	}
 	if(glfwVulkanSupported())
-		std::cout << "GLFW Supports Vulkan\n";
+		std::cout << "Note::InitializeVulkan( std::string ):void: GLFW Supports Vulkan\n";
 	else
-		std::cout << "GLFW Does Not Support Vulkan\n";
+		std::cout << "Error::InitializeVulkan( std::string ):void: GLFW Does Not Support Vulkan\n";
 }
-VkResult InitializeVulkanDebugLayer()
+VkResult InitializeVulkanDebugLayer( std::vector< const char* >&& validationLayers )
 {
-
-
-	glfwExtensions = glfwGetRequiredInstanceExtensions( &glfwExtensionCount );
-
-	std::vector< const char* > enabledExtensions( glfwExtensions, glfwExtensions + glfwExtensionCount );
-
 	VkDebugUtilsMessengerEXT debugMessenger;
+
 	VkDebugUtilsMessengerCreateInfoEXT debugMessengerCreateInfo = {};
 
 	debugMessengerCreateInfo.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
@@ -124,31 +127,37 @@ VkResult InitializeVulkanDebugLayer()
 	debugMessengerCreateInfo.pfnUserCallback = DebugCallBack;
 	debugMessengerCreateInfo.pUserData = nullptr;
 
-	std::vector< const char* > validationLayers{ "VK_LAYER_KHRONOS_validation" };
-
 	uint32_t layerCount;
 	vkEnumerateInstanceLayerProperties( &layerCount, nullptr );
 
 	std::vector< VkLayerProperties > availableLayers( layerCount );
 	vkEnumerateInstanceLayerProperties( &layerCount, availableLayers.data() );
 
-	createInfo.enabledLayerCount = 1;
-	createInfo.ppEnabledLayerNames = validationLayers.data();
+	for( const char* layerName : validationLayers )
+	{
+		bool layerFound = false;
+		for( const auto& layerProperties : availableLayers )
+		{
+			if( strcmp( layerName, layerProperties.layerName ) == 0 ) {
+				layerFound = true;
+				break;
+			}
+		}
 
-	enabledExtensions.push_back( VK_EXT_DEBUG_UTILS_EXTENSION_NAME );
-
-	createInfo.enabledExtensionCount = enabledExtensions.size();
-
-	createInfo.ppEnabledExtensionNames = enabledExtensions.data();
+		if( !layerFound ) {
+			std::cerr << "Error::InitializeVulkanDebugLayer():VkResult: Failed to confirm all layer names.\n";
+			return VkResult::VK_INCOMPLETE;
+		}
+	}
 
 
 	auto function = ( PFN_vkCreateDebugUtilsMessengerEXT ) vkGetInstanceProcAddr( instance, "vkCreateDebugUtilsMessengerEXT" );
 
 
-	if(function != nullptr) {
+	if( function != nullptr )
 		return function( instance, &debugMessengerCreateInfo, nullptr, &debugMessenger );
-	}
 	else {
+		std::cerr << "Error::InitializeVulkanDebugLayer():VkResult: vkCreateDebugUtilsMessengerEXT function is null.\n";
 		return VK_ERROR_EXTENSION_NOT_PRESENT;
 	}
 }
